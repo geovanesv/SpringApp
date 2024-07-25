0d62e62f1fb73c1520cb4f1114396473fd4f0b0a
geovanesv <geovanesv89@gmail.com>
2024-07-25 14:27:14 -0300

    Spring app

---
+++ src/main/java/com/example/login/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.login.repositories;
+
+import com.example.login.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
